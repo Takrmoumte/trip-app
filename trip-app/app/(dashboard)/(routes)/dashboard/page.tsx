aacec08224b8a03028fe5e786df78f659631e166
Karima Boukrouna <karima.boukrouna@gmail.com>
2023-08-18 12:38:50 +0200

    structured first folders: landing and dashboard

---
+++ trip-app/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { Button, buttonVariants } from "@/components/ui/button";
+
+const DashboardPage = () => {
+    return (
+        <h3>This is the dashboard page (protected)</h3>
+
+    )
+}
+
+export default DashboardPage;
---
+++ trip-app/app/(landing)/page.tsx
@@ -0,0 +1,10 @@
+import { Button, buttonVariants } from "@/components/ui/button";
+
+const LandingPage = () => {
+    return (
+        <h3>This is the landing page (unprotected)</h3>
+
+    )
+}
+
+export default LandingPage;
